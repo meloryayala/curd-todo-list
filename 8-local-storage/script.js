9bc4b07a7514b199e23c1b9c9b5e392122659926
Melory Ayala <melory.ayala@gmail.com>
2020-08-15 10:16:53 +0200

    Local storage

---
+++ 8-local-storage/script.js
@@ -0,0 +1,23 @@
+
+/*
+
+------------------Local storage-------------
+
+*/
+
+const tarefas = [
+    { tarefa: 'estudar js' },
+    { tarefa: 'estudar node.js' },
+    { tarefa: 'estudar react.js' },
+]
+
+const tarefasJson = JSON.stringify(tarefas)
+
+localStorage.setItem('tarefas', tarefasJson)
+
+
+const listaTarefasSalvas = localStorage.getItem('tarefas')
+
+const listaTarefasObj = JSON.parse(listaTarefasSalvas)
+
+console.log(listaTarefasObj)
